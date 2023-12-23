41f10af48e0d1e38f69b90eeacac6dd66af9278f
lemonilemon <imlemonilemon@gmail.com>
2023-12-23 13:53:52 +0000

    update bin2code

--- src/bin2code.py
+++ src/bin2code.py
@@ -1,4 +1,4 @@
-width = 512//16
+width = 256//16
 height = 256
 
 def toIntStr(s):
@@ -8,6 +8,7 @@ def toIntStr(s):
         return f"{int(s[1:], 2)}"
     return f"-{32768 - int(s[1:], 2)}"
 
+mux = input("Mux or not(y/n):")
 with open("bin.txt", "r") as f1:
     with open("bian.txt", "r") as f2:
         with open("code.txt", "w") as out:
@@ -20,12 +21,20 @@ with open("bin.txt", "r") as f1:
                     s1 = s1[::-1]
                     s2 = line2[c * 16 : (c + 1) * 16]
                     s2 = s2[::-1]
-                    if s2 == "0000000000000000":
-                        continue;
                     print(f"s1 = {s1}")
                     print(f"s2 = {s2}")
-                    out.write(f"let screenLocation[{cnt}] = Lib.mux(screenLocation[{cnt}], ")
-                    out.write(toIntStr(s1)); 
-                    out.write(", ");
-                    out.write(toIntStr(s2));
-                    out.write(");\n");
+                    if(mux == "y"):
+                        if s2 == "0000000000000000":
+                            continue;
+                        out.write(f"let screenLocation[{cnt}] = Lib.mux(screenLocation[{cnt}], ")
+                        out.write(toIntStr(s1)); 
+                        out.write(", ");
+                        out.write(toIntStr(s2));
+                        out.write(");\n");
+                    else:
+                        if s1 == "0000000000000000":
+                            continue;
+                        out.write(f"let screenLocation[{cnt}] = ")
+                        out.write(toIntStr(s1)); 
+                        out.write(";\n");
+
