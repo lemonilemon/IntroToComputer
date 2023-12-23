8e0afdcee06bb06bce4e02386f4b4c7cd888a136
lemonilemon <imlemonilemon@gmail.com>
2023-12-23 09:08:42 +0000

    script with bian

--- src/bin2code.py
+++ src/bin2code.py
@@ -1,19 +1,31 @@
-width = 224/16
-height = 56
-with open("bin.txt", "r") as f:
-    with open("code.txt", "w") as out:
-        for r in range(height):
-            line = f.readline()
-            for c in range(width): # 512 / 16
-                cnt = r * width + c
-                s = line[c * 16 : (c + 1) * 16]
-                s = s[::-1]
-                if s == "0000000000000000":
-                    continue
-                out.write(f"let screenLocation[{cnt}] = ")
-                if s == "1000000000000000":
-                    out.write(f"(~32767);\n")
-                elif s[0] == '0':
-                    out.write(f"{int(s[1:], 2)};\n")
-                else:
-                    out.write(f"-{32768 - int(s[1:], 2)};\n")
+width = 512//16
+height = 256
+
+def toIntStr(s):
+    if s == "1000000000000000":
+        return "(~32767)"
+    if s[0] == '0':
+        return f"{int(s[1:], 2)}"
+    return f"-{32768 - int(s[1:], 2)}"
+
+with open("bin.txt", "r") as f1:
+    with open("bian.txt", "r") as f2:
+        with open("code.txt", "w") as out:
+            for r in range(height):
+                line1 = f1.readline()
+                line2 = f2.readline()
+                for c in range(width): # 512 / 16
+                    cnt = r * 32 + c
+                    s1 = line1[c * 16 : (c + 1) * 16]
+                    s1 = s1[::-1]
+                    s2 = line2[c * 16 : (c + 1) * 16]
+                    s2 = s2[::-1]
+                    if s2 == "0000000000000000":
+                        continue;
+                    print(f"s1 = {s1}")
+                    print(f"s2 = {s2}")
+                    out.write(f"let screenLocation[{cnt}] = Lib.mux(screenLocation[{cnt}], ")
+                    out.write(toIntStr(s1)); 
+                    out.write(", ");
+                    out.write(toIntStr(s2));
+                    out.write(");\n");
